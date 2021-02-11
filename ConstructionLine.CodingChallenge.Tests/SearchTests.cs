f0531d3b4dc740f5cc76156852b960a3a7bc17e8
Majd <majdyafi@gmail.com>
2021-02-11 19:05:58 +0000

    Search engine and tests

---
+++ ConstructionLine.CodingChallenge.Tests/SearchTests.cs
@@ -0,0 +1,139 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        List<Shirt> _shirts;
+        SearchEngine _se;
+
+        public SearchTests()
+        {
+            _shirts = new List<Shirt>
+            {
+                new Shirt(Guid.Parse("DC1E8E26-2D52-471E-A7CA-2E2A33A5E074"), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.Parse("5AA962ED-2D54-41D2-B0F7-316CA4AB8843"), "Red - Medium", Size.Medium, Color.Red),
+                new Shirt(Guid.Parse("FE735FD2-096B-4310-9337-84F1CE835C0F"), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.Parse("3D288AAD-89EE-45FB-92D4-0C504BEED567"), "Blue - Medium", Size.Medium, Color.Blue),
+                new Shirt(Guid.Parse("C68C5789-8926-42F2-AD97-ABECBCCD4DA9"), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            _se = new SearchEngine(_shirts);
+        }
+
+        [Test]
+        [TestCase(nameof(Color.Black), 1)]
+        [TestCase(nameof(Color.Red), 2)]
+        [TestCase(nameof(Color.Blue), 2)]
+        public void SearchExistingByColour_Returns_ColourResults(string colour, int expected)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions { Colors = Color.All.Where(x => x.Name == colour).ToList() };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(expected, results.ColorCounts.Where(x => x.Color.Name == colour).First().Count);
+            Assert.AreEqual(colour, results.ColorCounts.Where(x => x.Count > 0).First().Color.Name);
+        }
+
+
+        [Test]
+        [TestCase(nameof(Color.Yellow))]
+        [TestCase(nameof(Color.White))]
+        public void SearchNonExistingByColour_Returns_ZeroColourResults(string colour)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions { Colors = Color.All.Where(x => x.Name == colour).ToList() };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(0, results.ColorCounts.Where(x => x.Color.Name == colour).First().Count);
+        }
+
+
+        [Test]
+        [TestCase(nameof(Size.Small), 1)]
+        [TestCase(nameof(Size.Medium), 3)]
+        public void SearchNonExistingBySize_Returns_ZeroColourResults(string size, int expected)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions { Sizes = Size.All.Where(x => x.Name == size).ToList() };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            Assert.AreEqual(expected, results.SizeCounts.Where(x => x.Size.Name == size).First().Count);
+        }
+
+        [Test]
+        [TestCase(nameof(Size.Medium), nameof(Color.Red))]
+        public void AsserMatchedShirts_Returns_Shirts(string size, string colour)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions
+            {
+                Sizes = Size.All.Where(x => x.Name == size).ToList(),
+                Colors = Color.All.Where(x => x.Name == colour).ToList()
+            };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            Assert.IsTrue(results.Shirts.Where(x => x.Color.Name == colour || x.Size.Name == size).Count() > 0);
+        }
+
+        [Test]
+        [TestCase("DC1E8E26-2D52-471E-A7CA-2E2A33A5E074")]
+        [TestCase("5AA962ED-2D54-41D2-B0F7-316CA4AB8843")]
+        [TestCase("FE735FD2-096B-4310-9337-84F1CE835C0F")]
+        [TestCase("3D288AAD-89EE-45FB-92D4-0C504BEED567")]
+        public void AsserMatchedShirts_Returns_MatcgedGuids(string guid)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions
+            {
+                Sizes = new List<Size> { Size.Medium },
+                Colors = new List<Color> { Color.Red }
+            };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            var guids = results.Shirts.Select(x => x.Id).ToList();
+            Assert.IsTrue(guids.Contains(Guid.Parse(guid)));
+        }
+
+        [Test]
+        [TestCase("C68C5789-8926-42F2-AD97-ABECBCCD4DA9")]
+        public void AsserNonMatchedShirts_Returns_MatcgedGuids(string guid)
+        {
+            //Assign
+            SearchOptions SO = new SearchOptions
+            {
+                Sizes = new List<Size> { Size.Medium },
+                Colors = new List<Color> { Color.Red }
+            };
+
+            //Action
+            var results = _se.Search(SO);
+
+            //Assert
+            var guids = results.Shirts.Select(x => x.Id).ToList();
+            Assert.IsFalse(guids.Contains(Guid.Parse(guid)));
+        }
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+        private readonly ConcurrentDictionary<Guid, List<Shirt>> shirtsByColorDictionary, shirtsBySizeDictionary;
+
+        /// <summary>
+        /// Create an instance of the Search Engine
+        /// </summary>
+        /// <param name="shirts">Complete list of shirst</param>
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts;
+
+            shirtsByColorDictionary =
+                new ConcurrentDictionary<Guid, List<Shirt>>(_shirts.GroupBy(x => x.Color.Id)
+                .ToDictionary(g => g.Key, g => g.ToList()));
+
+            shirtsBySizeDictionary =
+                new ConcurrentDictionary<Guid, List<Shirt>>(_shirts.GroupBy(x => x.Size.Id)
+                .ToDictionary(g => g.Key, g => g.ToList()));
+
+        }
+
+        /// <summary>
+        /// Search T-Shirts by Colour and Size
+        /// </summary>
+        /// <param name="options">T-Shirt Option Lists of Colours and Sizes</param>
+        /// <returns>Lists of matched T-Shirsts, Summary of Colours and Summary of Sizes yield by the search engine</returns>
+        public SearchResults Search(SearchOptions options)
+        {
+
+            var searchResults = Task.Run(async () => await SearchAsync(options));
+
+            searchResults.Wait();
+
+            return searchResults.Result;
+        }
+
+        /// <summary>
+        /// Attempts to speed up the search by utilising TPL 
+        /// </summary>
+        /// <param name="options">T-Shirt Option Lists of Colours and Sizes</param>
+        /// <returns>Lists of matched T-Shirsts, Summary of Colours and Summary of Sizes yield by the search engine</returns>
+        private async Task<SearchResults> SearchAsync(SearchOptions options)
+        {
+            Task<List<Shirt>> GetShirtsByColourOptionsTask = GetShirtsByColourOptions(options);
+            Task<List<Shirt>> GetShirtsBySizeOptionsTask = GetShirtsBySizeOptions(options);
+
+            await Task.WhenAll(GetShirtsByColourOptionsTask, GetShirtsBySizeOptionsTask)
+                .ConfigureAwait(false);
+
+            if (GetShirtsByColourOptionsTask.IsFaulted || GetShirtsBySizeOptionsTask.IsFaulted)
+            {
+                throw new Exception($"{GetShirtsByColourOptionsTask.Exception.Message} - {GetShirtsBySizeOptionsTask.Exception.Message}");
+            }
+
+            List<Shirt> shirts = await MergeResults(GetShirtsByColourOptionsTask, GetShirtsBySizeOptionsTask);
+
+            var ColorCounts = await ColorCountsResult(shirts, options);
+
+            var SizeCounts = await SizeCountsResult(shirts, options);
+
+            return new SearchResults
+            {
+                ColorCounts = ColorCounts,
+                SizeCounts = SizeCounts,
+                Shirts = shirts
+            };
+
+        }
+
+        /// <summary>
+        /// Merges the results returned by searching by colour and results returned by searching by size
+        /// </summary>
+        /// <param name="GetShirtsByColourOptionsTask">Results of matched T-Shirst by colour</param>
+        /// <param name="GetShirtsBySizeOptionsTask">Results of matched T-Shirst by size</param>
+        /// <returns></returns>
+        private static async Task<List<Shirt>> MergeResults(Task<List<Shirt>> GetShirtsByColourOptionsTask, Task<List<Shirt>> GetShirtsBySizeOptionsTask)
+        {
+            List<Shirt> shirts = GetShirtsByColourOptionsTask
+                .Result
+                .Union(GetShirtsBySizeOptionsTask.Result)
+                .ToList();
+            
+            return shirts;
+        }
+
+        /// <summary>
+        /// Get T-Shirst that have the size stipulated by size
+        /// </summary>
+        /// <param name="GetShirtsBySizeOptionsTask">near BigO(1) dictionary look-up for TShirst grouped by size</param>
+        /// <param name="options">indicates the sizes to look for</param>
+        /// <returns>matched T-shirst by size</returns>
+        private async Task<List<SizeCount>> SizeCountsResult(List<Shirt> GetShirtsBySizeOptionsTask, SearchOptions options)
+        {
+            var SizeCounts = new List<SizeCount>();
+            
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(Size.All, (size) =>
+                {
+                    SizeCounts.Add(new SizeCount
+                    {
+                        Size = size,
+                        Count = options.Sizes.Contains(size) ? 
+                            GetShirtsBySizeOptionsTask.Count(x => x.Size == size) 
+                            : 0
+                    });
+                });
+            });
+
+            return SizeCounts;
+        }
+
+        /// <summary>
+        /// Get summary of T-Shirst that have the size stipulated by colour
+        /// </summary>
+        /// <param name="GetShirtsBySizeOptionsTask">near BigO(1) dictionary look-up for TShirst grouped by colour</param>
+        /// <param name="options">indicates the colours to look for</param>
+        /// <returns>summary of matched T-shirst by size</returns>
+        private async Task<List<ColorCount>> ColorCountsResult(List<Shirt> GetShirtsByColourOptionsTask, SearchOptions options)
+        {
+            var ColorCounts = new List<ColorCount>();
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(Color.All, (colour) =>
+                {
+                    {
+                        ColorCounts.Add(new ColorCount
+                        {
+                            Color = colour,
+                            Count = options.Colors.Contains(colour) ?  
+                                GetShirtsByColourOptionsTask.Count(x => x.Color == colour) 
+                                : 0
+                        });
+                    }
+                });
+            });
+
+            return ColorCounts;
+        }
+
+        /// <summary>
+        /// Get summary of T-Shirst that have the size stipulated by colour
+        /// </summary>
+        /// <param name="GetShirtsBySizeOptionsTask">near BigO(1) dictionary look-up for TShirst grouped by colour</param>
+        /// <param name="options">indicates the colours to look for</param>
+        /// <returns>summary of matched T-shirst by size</returns>
+        private async Task<List<Shirt>> GetShirtsByColourOptions(SearchOptions searchOptions)
+        {
+
+            List<Shirt> results = new List<Shirt>();
+
+            foreach(var color in searchOptions.Colors) { 
+                if (shirtsByColorDictionary.TryGetValue(color.Id, out var matchedByColor)) 
+                { 
+                    results.AddRange(matchedByColor); 
+                }
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Get T-Shirst that have the size stipulated by size
+        /// </summary>
+        /// <param name="GetShirtsBySizeOptionsTask">near BigO(1) dictionary look-up for TShirst grouped by size</param>
+        /// <param name="options">indicates the sizes to look for</param>
+        /// <returns>matched T-shirst by size</returns>
+        private async Task<List<Shirt>> GetShirtsBySizeOptions(SearchOptions searchOptions)
+        {
+            List<Shirt> results = new List<Shirt>();
+
+            foreach (var size in searchOptions.Sizes)
+            {
+                if (shirtsBySizeDictionary.TryGetValue(size.Id, out var matchedBySize))
+                {
+                    results.AddRange(matchedBySize);
+                }
+            }
+
+            return results;
+        }
+    }
+}
